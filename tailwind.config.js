6e9a76ae59d8fa3c6167a399ef15b12553f907ee
sagarPakhrin <sagarlama826@gmail.com>
2021-02-09 22:21:15 +0545

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary:"#33445b"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
